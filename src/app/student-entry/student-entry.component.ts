e88b270cb6f3c2b39904fb98dd023c246a0177ee
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-11-28 23:23:03 +0530

    sss

--- src/app/student-entry/student-entry.component.ts
+++ src/app/student-entry/student-entry.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./student-entry.component.css']
 })
 export class StudentEntryComponent {
+ name=""
+ rno=""
+ adno=""
+ mno=""
+ cname=""
+ pname=""
+ pmno=""
+ uname=""
+ psw=""
+
+
+ readValues=()=>
+ {
+  let data:any={"name":this.name,
+                "rno":this.rno,
+                "adno":this.adno,
+                "mno":this.mno,
+                "cname":this.cname,
+                "pname":this.pname,
+                "pmno":this.pmno,
+                "uname":this.uname,
+                "psw":this.psw}
+
+
+ console.log(data)                
+                
+ }
 
 }
